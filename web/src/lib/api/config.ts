6ab9691262fce64b89138a6329b09b5996d206dd
Jantulík Daniel <daniel.jantulik@etyka.cz>
2023-07-25 01:27:58 +0200

    init 2

--- web/src/lib/api/config.ts
+++ web/src/lib/api/config.ts
@@ -1,7 +1,7 @@
 const API_CONFIG = {
     GET_PLACE_LIST: () =>  'http://localhost:8080/api/v1/catalog/place/finder',
     GET_PLACE_DETAIL: (placeId: string) =>  `http://localhost:8080/api/v1/catalog/place/${placeId}`,
-    GET_PLACE_PARAMETER: (placeId: string) =>  `http://localhost:8080/api/v1/catalog/parameters/${placeId}`,
+    // GET_PLACE_PARAMETER: (placeId: string) =>  `http://localhost:8080/api/v1/catalog/parameters/${placeId}`,
     FAVORITE_PLACES: () => 'http://localhost:8080/api/v1/catalog/place/favorite-places'
 };
 export default API_CONFIG;
